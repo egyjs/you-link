98b148ca4a63ff216cd82e920958636636f9259b
Abdulrahman elzahaby <el3zahaby@gmail.com>
2019-07-24 09:50:11 +0300

    Update v2.php

--- v2.php
+++ v2.php
@@ -1,6 +1,5 @@
-
 <?php 
-parse_str( parse_url( $_GET['url'], PHP_URL_QUERY ), $vars );
+    parse_str( parse_url( $_GET['url'], PHP_URL_QUERY ), $vars );
 
     parse_str(file_get_contents("https://www.youtube.com/get_video_info?video_id=".$vars['v']),$info);
     $streams = $info['adaptive_fmts']; // $info['url_encoded_fmt_stream_map'];
@@ -8,6 +7,6 @@ parse_str( parse_url( $_GET['url'], PHP_URL_QUERY ), $vars );
 
     $listQuality = [];
     foreach($streams as $stream){
-	    print_r($stream)
+	    print_r($stream);
     }
 
